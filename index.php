461c0424786a52c49b728565276c73dd41d88485
BON52391 <fabile1685@gmail.com>
2021-05-24 10:35:09 +0900

    [Add] index

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+      <head>
+            <meta charset="utf-8">
+            <title>test-site</title>
+      </head>
+      <body>
+            <p>This is test-site</p>
+            <p>The practice of github</p>
+            <?php
+            for ($i=0; $i <10 ; $i++) {
+                  echo $i."<br>";
+            }
+            ?>
+      </body>
+</html>
