1f678ac67ac04bf915015a574a47f3fc415c6561
BON52391 <fabile1685@gmail.com>
2021-05-24 11:49:19 +0900

    add register

---
+++ register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+      <head>
+            <meta charset="utf-8">
+            <title>sign-up</title>
+      </head>
+      <body>
+            <label>username</label>
+            <input type="text" name="" value="">
+            <label>password</label>
+            <input type="password" name="" value="">
+            <label>confirm password</label>
+            <input type="password" name="" value="">
+            <button type="button" name="button">sign-ip</button>
+      </body>
+</html>
