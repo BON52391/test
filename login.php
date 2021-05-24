1fec0110dda987d1d673823d107291bae0ae87b2
BON52391 <fabile1685@gmail.com>
2021-05-24 11:14:16 +0900

    [Add] login

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+      <head>
+            <meta charset="utf-8">
+            <title>login</title>
+      </head>
+      <body>
+            <form class="" action="index.html" method="post">
+                  <label>username</label>
+                  <input type="text" name="" value="">
+                  <label>password</label>
+                  <input type="password" name="" value="">
+                  <button type="" name="button">login</button>
+            </form>
+      </body>
+</html>
